342be4b30c19caf6f86c5e17362d62c84a524491
Sahedul Islam Rony <sahed.cse.just@gmail.com>
2024-09-18 00:24:20 +0600

    Added homepage

---
+++ CS50x/homepage/README.md
@@ -0,0 +1,3 @@
+# Portfolio
+
+- # [**Firebase**](https://sahedulislamrony.web.app)
---
+++ CS50x/homepage/js/script.js
@@ -0,0 +1,13 @@
+// Navigation Bar Activity Start
+
+
+let toggler = document.querySelector(".navbar button.navbar-toggler");
+
+    toggler.addEventListener('click',(e)=>{
+
+        toggler.classList.toggle("show");
+
+
+    });
+
+// Navigation Bar Activity End 
